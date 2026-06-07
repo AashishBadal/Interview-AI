02c0962ef66b81336615e562e995ee1720beb7c4
AashishBadal <aashishbadal4@gmail.com>
2026-06-07 17:10:08 +0545

    Change CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://interview-ai-cl6c.onrender.com',
     credentials: true
 }));
 
